86769e39c01ea7c323f4d9e3771dfec36edd9a01
Rocky3112 <alomgirhossainrocky@gmail.com>
2023-10-14 23:18:25 +0600

    used axios

--- src/Pages/Shared/Dashboard/AllUsers/AllUsers.jsx
+++ src/Pages/Shared/Dashboard/AllUsers/AllUsers.jsx
@@ -9,7 +9,61 @@ import { Helmet } from "react-helmet";
 
 
 const AllUsers = () => {
-  
+    const [axiosSecure] = useAxios();
+    const { data: users = [], refetch } = useQuery(['users'], async () => {
+        const res = await axiosSecure.get('/users')
+        return res.data;
+    })
+
+    const handleMakeAdmin = user =>{
+        fetch(`http://localhost:5000/users/admin/${user._id}`, {
+            method: 'PATCH'
+        })
+        .then(res => res.json())
+        .then(data => {
+            // console.log(data)
+            if(data.modifiedCount){
+                refetch();
+                Swal.fire({
+                    position: 'top-end',
+                    icon: 'success',
+                    title: `${user.name} is an Admin Now!`,
+                    showConfirmButton: false,
+                    timer: 1500
+                  })
+            }
+        })
+    }
+
+    const handleDelete=(user)=>{
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+          }).then((result) => {
+           fetch(`http://localhost:5000/users/${user._id}`, {
+            method:'DELETE'
+           })
+           .then(res=>res.json())
+           .then(data=>{
+            if(data.deletedCount > 0){
+                refetch();
+                Swal.fire(
+                    'Deleted!',
+                    'Your file has been deleted.',
+                    'success'
+                  )
+            }
+           })
+           
+          })
+      }
+    
+
     return (
         <div className="w-full">
             <Helmet>
