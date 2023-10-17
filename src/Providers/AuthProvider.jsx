f069e2a80a04ed75b273777fdc7adba6be257b3b
Rocky3112 <alomgirhossainrocky@gmail.com>
2023-10-17 23:34:40 +0600

    used jwt and secure

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -12,7 +12,9 @@ import {
   signOut,
   updateProfile,
 } from "firebase/auth";
+import axios from "axios";
 
+ 
 
 export const AuthContext = createContext(null);
 
@@ -51,17 +53,30 @@ const AuthProvider = ({ children }) => {
         });
     }
 
-    useEffect(()=>{
-        const unsubscribe= onAuthStateChanged(auth, currentUser =>{
-             // console.log('auth change', currentUser)
-             setUser(currentUser)
-             setLoading(false)
-             
-         })
-         return ()=>{
-             unsubscribe();
-         }
-     },[])
+    useEffect(() => {
+      const unsubscribe = onAuthStateChanged(auth, currentUser => {
+          setUser(currentUser);
+          console.log('current user', currentUser);
+
+          // get and set token
+          if(currentUser){
+              axios.post('http://localhost:5000/jwt', {email: currentUser.email})
+              .then(data =>{
+                  // console.log(data.data.token)
+                  localStorage.setItem('access-token', data.data.token)
+                  setLoading(false);
+              })
+          }
+          else{
+              localStorage.removeItem('access-token')
+          }
+
+          
+      });
+      return () => {
+          return unsubscribe();
+      }
+  }, [])
 
   const authInfo = {
     user,
