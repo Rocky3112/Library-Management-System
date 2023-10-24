744f8b38e44674e0dd996f0255ff1c4f268ac088
Rocky3112 <alomgirhossainrocky@gmail.com>
2023-10-24 23:03:50 +0600

    added add book page

--- src/Pages/Dashboard/AdminPanel/AddBook.jsx
+++ src/Pages/Dashboard/AdminPanel/AddBook.jsx
@@ -1,55 +1,58 @@
 
-import  { useState } from 'react';
-import axios from 'axios';
+// import  { useState } from 'react';
+// import axios from 'axios';
+import { useForm } from 'react-hook-form';
 // import './Addbooks.css'
 
 const AddBook = () => {
-  const [name, setName] = useState('');
-  const [author, setAuthor] = useState('');
-  const [message, setMessage] = useState('');
+  const { register, handleSubmit } = useForm();
+  // const [message, setMessage] = useState('');
 
-  const handleFormSubmit = async (e) => {
-    e.preventDefault();
+  const onSubmit = async (data) => {
 
-    try {
-      const response = await axios.post('/api/add-book', { name, author });
-      setMessage(response.data.message);
-    } catch (error) {
-      setMessage('Error: Book Already Exists');
-    }
+    console.log(data);
+    // e.preventDefault();
+
+    // try {
+    //   const response = await axios.post('/api/add-book', { name, author });
+    //   setMessage(response.data.message);
+    // } catch (error) {
+    //   setMessage('Error: Book Already Exists');
+    // }
   };
 
   return (
     <div className="text-center ">
       <span className="text-2xl font-semibold text-white">Add Books in Library</span>
-      <form onSubmit={handleFormSubmit}>
-        <div className="mt-4">
+      <form onSubmit={handleSubmit(onSubmit)} className="bg-slate-200 p-10 rounded shadow-xl">
+      <div className="flex items-center justify-between gap-6">
+        <div className="grid gap-5">
+
+          <label htmlFor="name">Name:</label>
           <input
             type="text"
-            name="name"
-            placeholder="Enter Book Name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            className="w-80 py-3 px-4 border rounded"
-            required
+            id="name"
+            {...register("name", { required: true })}
           />
-        </div>
-        <div className="mt-4">
+
+          <label htmlFor="author">Author Name:</label>
           <input
             type="text"
-            name="auth"
-            placeholder="Enter Author Name"
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
-            className="w-80 py-3 px-4 border rounded"
-            required
+            id="author"
+            {...register("author", { required: true })}
           />
         </div>
-        <div className="mt-4">
-          <input type="submit" value="ADD BOOK" className="hover:bg-blue-500 text-black bg-white py-3 px-4 rounded" />
-        </div>
-      </form>
-      <div className="mt-4 text-green-600">{message}</div>
+      </div>
+
+      <div className="mt-4">
+        <input
+          type="submit"
+          value="ADD BOOK"
+          className="hover:bg-blue-500 text-black bg-white py-3 px-4 rounded"
+        />
+      </div>
+    </form>
+      {/* <div className="mt-4 text-green-600">{message}</div> */}
       <a href="adminhome" className="text-white mt-4">Go Back</a>
     </div>
   );
